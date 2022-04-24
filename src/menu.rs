9e797c10579d7e82149eee16075bb76364229b7a
Albin Dalbert <albin.dalbert.all@gmail.com>
2022-04-24 20:13:24 +0200

    Update menu.rs

--- src/menu.rs
+++ src/menu.rs
@@ -1,5 +1,30 @@
-
+/// A general purpose menu for picking a function to run.
+/// 
 struct Menu {
     name: String,
     items: Vec<(String, fn())>,
+}
+
+impl Menu {
+    pub fn new(name: String) -> Menu {
+        Menu {
+            name,
+            items: Vec::new(),
+        }
+    }
+
+    pub fn add_item(&mut self, name: String, func: fn()) {
+        self.items.push((name, func));
+    }
+
+    pub fn run(&self) {
+        let mut i: i16 = 0;
+        for (name, func) in &self.items {
+            println!("{0: <10}) {1: <100}", i, name);
+            i += 1;
+        }
+
+        let inp = input().parse::<i32>().unwrap_or(-1);
+        self.items[inp as usize].1();
+    }
 }
